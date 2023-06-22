e9938bbba356e7a56d2216de9a1a1a052ec62a4b
chrisaboyd <102701870+chrisaboyd@users.noreply.github.com>
2023-06-22 09:17:33 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -3,8 +3,10 @@ from prefect import get_run_logger
 import time
 import pandas as pd
 
+
 def this_is_not_a_task(logger):
     logger.info("I am not a task context")
+
 
 @task
 def log_platform_info():
@@ -13,27 +15,10 @@ def log_platform_info():
     this_is_not_a_task(logger)
 
 
-@task
-def read_file(path):
-    return pd.read_csv(path)
-
-
-@task(log_prints=True)
-def transform_pd(df):
-    results = [ row['col1'] * row['col2'] for index,row in df.iterrows() ]
-    print (results)
-    #for index,row in df.iterrows():
-    #    print (row['col1'] * row['col2'])
-
-
 @flow(log_prints=True)
 def hello_world():
     logger = get_run_logger()
     log_platform_info()
-
-    csv_path = "./file.csv"
-    df = read_file(csv_path)
-    transform_pd(df)
 
 
 if __name__ == "__main__":
